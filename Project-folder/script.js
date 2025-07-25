d7614dde35361db2ec0b55191f74247990cce35b
Falmata-LK123 <falmatalawankachallah@gmail.com>
2025-07-25 03:09:50 -0700

    Added new Sections and features to the HTML and CSS files

--- Project-folder/script.js
+++ Project-folder/script.js
@@ -7,38 +7,4 @@
             mobileMenu.classList.toggle('active')
         );
         });
-        // FAQ Accordion
-document.addEventListener('DOMContentLoaded', () => {
-    const faqContainer = document.querySelector('.faq-content');
-  
-    faqContainer.addEventListener('click', (e) => {
-      const mainH = e.target.closest('.faq-main-h');
-  
-      if (!mainH) return;
-  
-      const main = mainH.parentElement;
-      const mainB = main.querySelector('.faq-main-b');
-      const icon = mainH.querySelector('i');
-  
-      // Toggle icon
-      icon.classList.toggle('fa-plus');
-      icon.classList.toggle('fa-minus');
-  
-      // Toggle visibility of body
-      mainB.classList.toggle('open');
-  
-      // Close other open FAQ bodies
-      const otherMains = faqContainer.querySelectorAll('.faq-main');
-  
-      otherMains.forEach((otherGroup) => {
-        if (otherMain!== main) {
-          const otherMainB = otherGroup.querySelector('.faq-main-b');
-          const otherIcon = otherMain.querySelector('.faq-main-h i');
-  
-          otherMainB.classList.remove('yes');
-          otherIcon.classList.remove('fa-minus');
-          otherIcon.classList.add('fa-plus');
-        }
-      });
-    });
-  });
+      
